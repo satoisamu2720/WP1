1849d7c8bb2ad2eb873e87b9810e6567e9451c30
and lloyd <andlloyd.hotaru.2720@gmail.com>
2024-11-04 18:43:36 +0900

    ok

--- Assets/Camera/MouseLook.cs
+++ Assets/Camera/MouseLook.cs
@@ -7,6 +7,7 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity;  //感度
     public Transform player;
     private float xRotation = 0f;
+    private float yRotation = 0f;
 
     void Start()
     {
@@ -21,12 +22,16 @@ public class MouseLook : MonoBehaviour
 
         // 上下運動
         xRotation -= mouseY;
+        yRotation += mouseX;
+        
 
         // 上下運動（移動できる角度）に制限を加える。
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        yRotation = Mathf.Clamp(-90f, yRotation, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
 
         // 左右運動
-        player.Rotate(Vector3.up * mouseX);
+        //player.Rotate(Vector3.up * mouseX);
     }
 }
